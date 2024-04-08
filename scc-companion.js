9db61b9fd16b6c21b742b1cf524fa21bd4d7e6d1
Christopher Hofmann <cwh@suse.de>
2024-04-08 17:47:39 +0200

    First release: Find ticket reporter in SCC

---
+++ scc-companion.js
@@ -0,0 +1,14 @@
+const reporter_elem = document.querySelector("#reporter-val > span.user-hover");
+const reporter_email = reporter_elem.getAttribute("rel");
+
+const scc_button = document.createElement("img");
+scc_button.setAttribute("src", browser.runtime.getURL("icons/scc-24.png"));
+scc_button.setAttribute("style", "border-radius: 50%; vertical-align: bottom;");
+scc_button.className = "scc-button";
+
+const scc_link = document.createElement("a");
+scc_link.setAttribute("href", "https://scc.suse.com/admin/users?query=" + reporter_email);
+scc_link.setAttribute("title", `Find ${reporter_email} in SCC …`);
+scc_link.append(scc_button);
+
+reporter_elem.after(scc_link);
